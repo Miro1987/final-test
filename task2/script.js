9d710d0cbeeabdb0ebbf09c5f21b28515ce8e10c
krisisim <krisisim@abv.bg>
2017-01-09 21:38:32 +0200

    Final test

--- task2/script.js
+++ task2/script.js
@@ -1,18 +1,30 @@
 "use strict"
 
 // задача 1
-
+arr.sort(function (a, b) {
+    return a > b;
+});
+console.log(arr);
 var arr = [2, 5, 8, 4, 1, 12];
 
 // задача 2
 
 var text = "The quick brown fox jumps over the lazy dog";
+var list = text.split(" ");
 
-// задача 3
+function words(list) {
+    return list.length >= 4
+        // задача 3
+
+   
+    // задача 4
 
 
-// задача 4
+    // задача 5
+     var sessionKey = getCookie("samplename");
 
-
-// задача 5
-
+    if (sessionKey == null) {
+        alert("cookie does not exist");
+    } else {
+        alert("cookie exists");
+    }
